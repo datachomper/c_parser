6c9459d0f04f291c0c8652cb2c72129b5b00483f
Russ Meyerriecks <datachomper@gmail.com>
2014-12-16 17:14:57 -0600

    functionized tokenizer

--- parse.c
+++ parse.c
@@ -7,18 +7,51 @@
 //TODO: Find the largest token string size and set this properly
 #define MAXTOKENLEN 127
 
-struct token {
+struct Token {
 	char string[MAXTOKENLEN];
 };
 
-struct token tstack[255];
+struct Token tstack[255];
+
+/* Pass: String and token struct */
+/* Returns: Next token in string or NULL */
+/* 	    Increments s past returned token */
+int get_token(char **s, struct Token *t) {
+	char *x = t->string;
+
+	/* Skip past spaces */
+	while (isblank(**s)) { (*s)++; }
+	
+	/* Return false at EOL */
+	if (**s == '\0')
+		return -1;
+
+	/* Single char symbol detection */
+	if (!isalnum(**s)) {
+		*x++ = **s;
+		(*s)++;
+	} else {
+		/* Multi-char symbol detection */
+		while (isalnum(**s) && (**s != '\0') && !isblank(**s)) {
+			*x++ = **s;
+			(*s)++;
+		}
+	}
+	
+	*x = '\0';
+
+	return 0;
+}
 
 int main(int argc, char **argv)
 {
+	//TODO: Remove comment block in order to pass in strings
+	#if 0
 	if (argc < 2) {
 		printf("Usage: %s \"crazy c declaration\"\n", argv[0]);
 		return -1;
 	}
+	#endif
 
 	/* Print string to be parsed */
 	char *line = argv[1];
@@ -26,16 +59,17 @@ int main(int argc, char **argv)
 	line = "int *((*fp)[10])()";
 	printf("Parsing %s\n", line);
 	
+	struct Token *sp = &tstack[0];
+	while (!get_token(&line, sp++));
 
+#if 0
 	/* Push tokens onto stack from left to right until first identifier is read */ 
 	char *c = line;
 	int x = 0;
-	struct token *sp = &tstack[0];
+	struct Token *sp = &tstack[0];
 	for (int i = 0; c[i] != 0; i++) {
 		if (isalnum(c[i])) {
 			// It's a word symbol or identifier
-			printf("alpha: %c\n", c[i]);
-			printf("char: %c x: %d\n", c[i], x);
 			sp->string[x] = c[i];
 			x++;
 		} else {
@@ -44,19 +78,18 @@ int main(int argc, char **argv)
 
 			/* Finish string if we were tracking one with x */
 			if (x) {
-				printf("char: %c x: %d\n", c[i], x);
 				sp->string[x] = 0;
 				x = 0;
 				sp++;
 			}
 
 			// It's one of *()[]
-			printf("not alpha: %c\n", c[i]);
 			sp->string[0] = c[i];
 			sp->string[1] = 0;
 			sp++;
 		}
 	}
+#endif
 
 	/* DEBUG: Print out all symbols on the stack */
 	printf("Dumping stack:\n");
