5b0c98c72de85bb71b909ed02eac7350d4fd051b
Russ Meyerriecks <datachomper@gmail.com>
2014-12-17 20:51:50 -0600

    Basic translation of default string works!

--- parse.c
+++ parse.c
@@ -3,15 +3,40 @@
 
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 //TODO: Find the largest token string size and set this properly
 #define MAXTOKENLEN 127
+#define NUM_SYMBOLS 20
+
+char symbols[NUM_SYMBOLS][MAXTOKENLEN] = {"short", "char", "int","long","double","float","unsigned","signed",
+					  "volatile", "extern", "static", "const", "struct", "enum", "union"};
 
 struct Token {
 	char string[MAXTOKENLEN];
+	enum {NOT_IDENT, IDENT} class;
 };
 
 struct Token tstack[255];
+
+void classify_token(struct Token *t) {
+	/* Quick classify single char tokens */
+	if (t->string[1] == '\0') {
+		t->class = NOT_IDENT;
+		return;
+	}
+	
+	/* Handle alphanumeric tokens */
+	for (int i = 0; i < NUM_SYMBOLS; i++) {
+		if (!strcmp(t->string, symbols[i])) {
+			t->class = NOT_IDENT;
+			return;
+		}
+	}
+
+	/* No matches on known static symbols, must be an identifier */
+	t->class = IDENT;
+}
 
 /* Pass: String and token struct */
 /* Returns: Next token in string or NULL */
@@ -39,6 +64,7 @@ int get_token(char **s, struct Token *t) {
 	}
 	
 	*x = '\0';
+	classify_token(t);
 
 	return 0;
 }
@@ -56,47 +82,65 @@ int main(int argc, char **argv)
 	/* Print string to be parsed */
 	char *line = argv[1];
 	//TODO: Remove demo line
-	line = "int *((*fp)[10])()";
+	line = "char* const *(*next)()";
 	printf("Parsing %s\n", line);
 	
+	/* From left to right push all tokens to the stack until the first
+	 * identifier is found */
 	struct Token *sp = &tstack[0];
-	while (!get_token(&line, sp++));
-
-#if 0
-	/* Push tokens onto stack from left to right until first identifier is read */ 
-	char *c = line;
-	int x = 0;
-	struct Token *sp = &tstack[0];
-	for (int i = 0; c[i] != 0; i++) {
-		if (isalnum(c[i])) {
-			// It's a word symbol or identifier
-			sp->string[x] = c[i];
-			x++;
-		} else {
-			/* Skip spaces? */
-			if (c[i] == ' ') continue;
-
-			/* Finish string if we were tracking one with x */
-			if (x) {
-				sp->string[x] = 0;
-				x = 0;
-				sp++;
-			}
-
-			// It's one of *()[]
-			sp->string[0] = c[i];
-			sp->string[1] = 0;
-			sp++;
-		}
-	}
-#endif
-
-	/* DEBUG: Print out all symbols on the stack */
-	printf("Dumping stack:\n");
-	sp = &tstack[0];
-	while (sp->string[0]) {
-		printf("%s ", sp->string);
+	while (!get_token(&line, sp)) {
+		if (sp->class == IDENT)
+			break;
 		sp++;
+	}
+
+	printf("%s is a ", sp->string);
+
+	/* Main decoder loop */
+	while (1) {
+		struct Token this;
+
+		/* First move to the right */
+		if (!get_token(&line, &this)) {
+			if (this.string[0] == '[') {
+				printf("array of ");
+			}
+			if (this.string[0] == '(') {
+				while (this.string[0] != ')')
+					get_token(&line, &this);
+				printf("a function returning ");
+				continue;
+			}
+		}
+
+		/* Then move to the left */
+		moveleft:
+		sp--;
+
+		if (sp->string[0] == '(') {
+			while (this.string[0] != ')')
+				get_token(&line, &this);
+			continue;
+		}
+
+		/* If const, volatile, or *, continue reading left */
+		if (sp->string[0] == '*') {
+			printf("pointer to ");	
+			goto moveleft;
+		} else if (!strcmp(sp->string, "volatile")) {
+			printf("volatile ");	
+			goto moveleft;
+		} else if (!strcmp(sp->string, "const")) {
+			printf("read-only ");	
+			goto moveleft;
+		} else {
+			/* Remaining tokens form basic declaration */
+			printf("%s ", sp->string);
+		}
+
+		/* If stack pointer hits the beginning of the stack, exit */
+		if (sp == &tstack[0])
+			break;
 	}
 
 	return 0;
